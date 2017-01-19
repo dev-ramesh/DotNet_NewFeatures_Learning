0bc2d1f9d9d7962f6157166cf3a370e44c8f56ed
ramesh <rameshp89@yahoo.co.in>
2017-01-19 00:55:26 +0000

    Asp.net Core with MVC implementation

---
+++ TheWorld/Controller/Web/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        
+    }
+}
